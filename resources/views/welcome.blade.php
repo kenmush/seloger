505577a2c3ff83be4f5b299b830d67ea6b683af1
Kennedy Mutisya <kenmsh@gmail.com>
2019-11-25 12:52:50 +0300

    Responsive table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,50 +2,52 @@
 @section('content')
     <div class="row">
         <div class="container">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Listings</h4>
-                        <div>
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered">
-                                    <thead class="thead-light">
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Website</th>
-                                        <th>Postcode</th>
-                                        <th>URL</th>
-                                        <th>price/m2</th>
-                                        <th>price/room</th>
-                                        <th>price/bedroom</th>
-                                        <th>location</th>
-                                        <th>type</th>
-                                        <th>m2</th>
-                                        <th># of rooms</th>
-                                        <th># of bedrooms</th>
-                                        <th>Description.</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach(\App\Results::all() as $result)
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Listings</h4>
+                            <div>
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered">
+                                        <thead class="thead-light">
                                         <tr>
-                                            <td scope="row">{{$loop->iteration }}</td>
-                                            <td>{{ $result->website }}</td>
-                                            <td>{{ $result->postcode }}</td>
-                                            <td><a href="{{ $result->url }}">
-                                                    {{ \Illuminate\Support\Str::afterLast($result->url,'/') }}
-                                                </a></td>
-                                            <td>{{ $result->price }}</td>
-                                            <td>{{ $result->location }}</td>
-                                            <td>{{ $result->type }}</td>
-                                            <td>{{ $result->m2 }}</td>
-                                            <td>{{ $result->rooms }}</td>
-                                            <td>{{ $result->bedrooms }}</td>
-                                            <td>{{ $result->description }}</td>
+                                            <th>#</th>
+                                            <th>Website</th>
+                                            <th>Postcode</th>
+                                            <th>URL</th>
+                                            <th>price/m2</th>
+                                            <th>price/room</th>
+                                            <th>price/bedroom</th>
+                                            <th>location</th>
+                                            <th>type</th>
+                                            <th>m2</th>
+                                            <th># of rooms</th>
+                                            <th># of bedrooms</th>
+                                            <th>Description.</th>
                                         </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                        @foreach(\App\Results::all() as $result)
+                                            <tr>
+                                                <td scope="row">{{$loop->iteration }}</td>
+                                                <td>{{ $result->website }}</td>
+                                                <td>{{ $result->postcode }}</td>
+                                                <td><a href="{{ $result->url }}">
+                                                        {{ \Illuminate\Support\Str::afterLast($result->url,'/') }}
+                                                    </a></td>
+                                                <td>{{ $result->price }}</td>
+                                                <td>{{ $result->location }}</td>
+                                                <td>{{ $result->type }}</td>
+                                                <td>{{ $result->m2 }}</td>
+                                                <td>{{ $result->rooms }}</td>
+                                                <td>{{ $result->bedrooms }}</td>
+                                                <td>{{ $result->description }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
