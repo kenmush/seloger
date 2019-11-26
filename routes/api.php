3147245a8644c09b81e9939aa77faefc54b0c0ec
Kennedy Mutisya <kenmsh@gmail.com>
2019-11-26 23:36:14 +0300

    Add proxy to access data

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,6 @@
 use Illuminate\Http\Request;
 
 
-Route::middleware('auth:api')->get('/search', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['prefix'=>'v1'], function () {
     Route::resource('city', 'CityController');
 });
