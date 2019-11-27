fced4007d693903443d36de18bd1e099a85c9dd5
Kennedy Mutisya <kenmsh@gmail.com>
2019-11-27 18:53:04 +0300

    send emails with less than 3000 square metres

--- resources/views/mail/sendexcel.blade.php
+++ resources/views/mail/sendexcel.blade.php
@@ -6,7 +6,8 @@ Attached finding the listing report for {{ today()->toFormattedDateString() }} a
     | Zip Code       | p/m2         | m2  |  URL  |
     | ------------- |:-------------:| --------:| --------:|
     @foreach(\App\Results::where('squareMeterPrice', '<', 3000)->get() as $listing)
-    | {{ $listing->postcode }}      | {{ $listing->squareMeterPrice }}      | {{ $listing->m2 }}      |  {{ $listing->url }}         |
+    | {{ $listing->postcode }}      | {{ $listing->squareMeterPrice }}      | {{ $listing->m2 }}      | [View Listing]({{$listing->url}})
+       |
     @endforeach
 @endcomponent
 
