27d93e250d7dfbb5d98aa9bf2905b53dfece4a5f
Kennedy Mutisya <kenmsh@gmail.com>
2019-11-27 18:55:53 +0300

    send emails with less than 3000 square metres

--- resources/views/mail/sendexcel.blade.php
+++ resources/views/mail/sendexcel.blade.php
@@ -5,12 +5,14 @@ Attached finding the listing report for {{ today()->toFormattedDateString() }} a
 @component('mail::table')
     | Zip Code       | p/m2         | m2  |  URL  |
     | ------------- |:-------------:| --------:| --------:|
-    @foreach(\App\Results::where('squareMeterPrice', '<', 3000)->get() as $listing)
+    @foreach(\App\Results::where('squareMeterPrice', '<', 3000)->take(15)->get() as $listing)
     | {{ $listing->postcode }}      | {{ $listing->squareMeterPrice }}      | {{ $listing->m2 }}      | [View Listing]({{$listing->url}})
        |
     @endforeach
 @endcomponent
-
+@component('mail::button', ['url' => 'http://3.14.251.242/login', 'color' => 'success'])
+    View more listings
+@endcomponent
 
 Thanks,<br>
 {{ config('app.name') }}
