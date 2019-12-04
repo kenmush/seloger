94461ce0818e8dd3c8c1244317a2c173c7821817
Kennedy Mutisya <kenmsh@gmail.com>
2019-12-04 11:55:48 +0300

    Seloger proxy

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(ScrapeSelonger::class)->hourly();
-        $schedule->command(SendPersonalEmail::class)->hourly();
+//        $schedule->command(SendPersonalEmail::class)->hourly();
     }
 
     /**
